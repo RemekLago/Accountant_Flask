59ce1f5a120aebfec44dffa0a91ed7f6366c8d19
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-04-18 13:37:16 +0200

    added conditions to sell and purchse process

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+from datetime import date
 from app import app, db
 from flask import render_template, url_for, flash, redirect, request
-from app.forms import SaleForm, BuyForm, SaldoForm
-from app.models import StockTable, SaldoTable, HistoryTable
+from app.forms import SaleForm, BuyForm, SaldoForm, HistoryForm
+from app.models import StockTable, SaldoTable, HistoryTable, CustomHistoryTable
 
 
 @app.route('/')
@@ -10,50 +11,44 @@ from app.models import StockTable, SaldoTable, HistoryTable
 def index():
     today = datetime.now().date()
     n = len(SaldoTable.query.all())
-    current_saldo = (SaldoTable.query.filter(SaldoTable.id==n).all())[0].saldo
+    if n == 0:
+        current_saldo = 0
+    else:
+        current_saldo = (SaldoTable.query.filter(SaldoTable.id==n).all())[0].saldo
     return render_template('index.html', today=today, current_saldo=current_saldo)
 
 @app.route('/sale', methods=["GET", "POST"])
 def sale():
     today = datetime.now().date()
     form = SaleForm()
+    product_stock = StockTable.query.all()
+    
     n = len(SaldoTable.query.all())
     if n == 0:
         saldo_last = 0
     else: 
         saldo_last = (SaldoTable.query.filter(SaldoTable.id==n).all())[0].saldo
-    print(n)
-    print(saldo_last)
-    # print(form.validate_on_submit())
-    # print(form.is_submitted(), form.validate())
-    # if request.method == 'POST':
-    #     product = request.form['product']
-    #     quantity = request.form['quantity']
-    #     price = request.form['price']
-    #     sale1 = StockTable(
-    #         product = product,   
-    #         quantity = quantity,  
-    #         price = price,   
-    #         )
-    #     sale2 = SaldoTable(
-    #         saldo = -(price * quantity),  
-    #         )
-    #     sale3 = HistoryTable(
-    #         name = "sale",
-    #         product = product,   
-    #         quantity = quantity,  
-    #         price = price,
-    #         # comment = null
-    #         )
+        
     if form.validate_on_submit():
-        sale1 = StockTable(
-            product = form.product.data,   
-            quantity = form.quantity.data,  
-            price = form.price.data,   
-            )
+        if StockTable.query.filter(StockTable.product == form.product.data).all():
+            current_quantity_product = StockTable.query.filter(StockTable.product == form.product.data).all()[0].quantity
+            if current_quantity_product >= form.quantity.data:
+                sale1 = StockTable(
+                    product = form.product.data,   
+                    quantity = current_quantity_product - form.quantity.data,  
+                    )
+                a = StockTable.query.filter_by(product = form.product.data).one()
+                db.session.delete(a)
+            else:
+                print("Error, not enought quantity products on stock")    
+        else: 
+            sale1 = StockTable(
+                product = form.product.data,   
+                quantity = form.quantity.data,  
+                )
         sale2 = SaldoTable(
             payment = (form.price.data * form.quantity.data),
-            saldo = int(saldo_last) + (form.price.data * form.quantity.data),
+            saldo = int(saldo_last) + int(form.price.data * form.quantity.data),
             status = "sell" 
             )
         sale3 = HistoryTable(
@@ -81,14 +76,23 @@ def purchase():
         saldo_last = 0
     else: 
         saldo_last = (SaldoTable.query.filter(SaldoTable.id==n).all())[0].saldo
-    print(n)
-    print(saldo_last)
     if form.validate_on_submit():
-        purchase1 = StockTable(
-            product = form.product.data,   
-            quantity = form.quantity.data,  
-            price = form.price.data,   
-            )
+        if StockTable.query.filter(StockTable.product == form.product.data).all():
+            current_quantity_product = StockTable.query.filter(StockTable.product == form.product.data).all()[0].quantity
+            purchase1 = StockTable(
+                product = form.product.data,   
+                quantity = form.quantity.data + current_quantity_product,     
+                )
+            a = StockTable.query.filter_by(product = form.product.data).one()
+            db.session.delete(a)
+            print(current_quantity_product)
+            print(purchase1.quantity)
+        else: 
+            purchase1 = StockTable(
+                product = form.product.data,   
+                quantity = form.quantity.data,    
+                )
+            
         purchase2 = SaldoTable(
             payment = -(form.price.data * form.quantity.data),
             saldo = int(saldo_last) + (form.price.data * form.quantity.data),
@@ -128,7 +132,7 @@ def payment():
                 saldo = int(saldo_last) + int(form.saldo.data),
                 )
         elif int(form.saldo.data) < 0 and (int(form.saldo.data) + int(saldo_last)) >= 0:
-                        payment2 = SaldoTable(
+            payment2 = SaldoTable(
                 payment = form.saldo.data,
                 status = "payment on account",
                 saldo = int(saldo_last) + int(form.saldo.data),
@@ -149,7 +153,45 @@ def payment():
         return redirect(url_for('payment'))
     return render_template('payment.html', title='Payment', form=form, today=today, saldo_last=saldo_last)
 
-@app.route('/history')
+@app.route('/custom_history', methods=["GET", "POST"])
+def custom_history():
+    form = HistoryForm()
+    today = datetime.now().date()
+    ()
+    if request.method == 'POST':
+        a = request.form['period_from'].split('-')
+        b = request.form['period_to'].split('-')
+        period_from = date(int(a[0]), int(a[1]), int(a[2]))
+        period_to = date(int(b[0]), int(b[1]), int(b[2]))
+        history1 = CustomHistoryTable(
+            period_from = period_from,
+            period_to = period_to,
+            )
+        
+        db.session.query(CustomHistoryTable).delete()
+        db.session.add(history1)
+        db.session.commit()
+        
+        flash('Congratulations, task completed')
+        return redirect(url_for('custom_history_period'))
+    return render_template('custom_history.html', title='Custom History', form=form, today=today)
+
+@app.route('/custom_history_period', methods=["GET", "POST"])
+def custom_history_period():
+    today = datetime.now().date()
+    period_from = CustomHistoryTable.query.get(1).period_from
+    period_to = CustomHistoryTable.query.get(1).period_to + timedelta(days = 1)
+    # history = HistoryTable.query.all()
+    history = HistoryTable.query.filter((HistoryTable.date>=period_from) & (HistoryTable.date<=(period_to))).all()
+    print(period_from)
+    print(type(period_from))
+    print(period_to)
+    print(history)
+
+    return render_template('custom_history_period.html', title='Custom History', today=today, history=history)
+
+
+@app.route('/history', methods=["GET", "POST"])
 def history():
     today = datetime.now().date()
     history = HistoryTable.query.all()
@@ -165,4 +207,6 @@ def stock():
 def saldo():
     today = datetime.now().date()
     saldo = SaldoTable.query.all()
-    return render_template('saldo.html', title='Saldo', saldo=saldo, today=today)
+    n = len(SaldoTable.query.all())
+    current_saldo = (SaldoTable.query.filter(SaldoTable.id==n).all())[0].saldo
+    return render_template('saldo.html', title='Saldo', saldo=saldo, today=today, current_saldo=current_saldo)
