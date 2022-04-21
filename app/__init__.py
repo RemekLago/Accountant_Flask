f12b652d0b5e71922b294b2fa38bd52223338cb7
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-04-21 12:53:45 +0200

    update

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from socket import gethostname
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -9,3 +10,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from app import routes, models
+
+if __name__ == '__main__':
+    db.create_all()
+    if 'liveconsole' not in gethostname():
+        app.run()
